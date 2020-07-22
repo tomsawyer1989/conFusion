1c73e7d7c1f7979ad9ed8f09990467e6e2af95ae
Jorge Leonardo Herrera Ortiz <herrera.jorge@correounivalle.edu.co>
2020-07-22 16:11:31 -0500

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('toggle');
+    });
+
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('toggle');
+    });
+    
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
